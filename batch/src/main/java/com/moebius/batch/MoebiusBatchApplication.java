97eb7cdd3a9d941582637cb03c74562b60074c6e
Knunu(김선우) <knunu@coupang.com>
2018-12-04 21:45:32 +0900

    [MBS-001] Initialized batch project with gradle setup

---
+++ batch/src/main/java/com/moebius/batch/MoebiusBatchApplication.java
@@ -0,0 +1,12 @@
+package com.moebius.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoebiusBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoebiusBatchApplication.class, args);
+	}
+}
