5a1276e7507dd80718f47dae707da413dbc4d17d
Knunu <burette@hanyang.ac.kr>
2019-02-11 01:04:13 +0900

    [MBS-7] Add dependency of reactivestreams & extend reactive mongo config.

--- backend/src/main/java/com/moebius/backend/BackendContextLoader.java
+++ backend/src/main/java/com/moebius/backend/BackendContextLoader.java
@@ -1,13 +1,27 @@
 package com.moebius.backend;
 
 import com.moebius.backend.database.Repositories;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Slf4j
 @Configuration
 @EnableReactiveMongoRepositories(basePackageClasses = Repositories.class)
-public class BackendContextLoader {
+public class BackendContextLoader extends AbstractReactiveMongoConfiguration {
 
+    @Bean
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create(); // TODO : change to separated mongo db endpoint later.
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return null;
+    }
 }
