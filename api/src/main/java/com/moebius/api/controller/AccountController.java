3cbefefde6999fea4b366a6ed5e26bc449260685
Knunu <burette@hanyang.ac.kr>
2019-03-13 23:59:19 +0900

    [MBS-10] [WIP] Implement login ...

--- api/src/main/java/com/moebius/api/controller/AccountController.java
+++ api/src/main/java/com/moebius/api/controller/AccountController.java
@@ -1,18 +1,13 @@
 package com.moebius.api.controller;
 
 import com.moebius.api.dto.AccountResponseDto;
-import com.moebius.api.dto.LoginDto;
 import com.moebius.api.dto.UserDto;
 import com.moebius.backend.account.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.security.Principal;
@@ -32,7 +27,7 @@ public class AccountController {
 	}
 
 	@PostMapping("/")
-	public Mono<AccountResponseDto> login(@RequestBody LoginDto loginDto) {
+	public Mono<AccountResponseDto> login(ServerWebExchange serverWebExchange) {
 
 		return null;
 	}
