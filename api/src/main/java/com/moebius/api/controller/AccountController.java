edc82bd5dd0b4c97f71decda0a79c17f0646fc58
Knunu(김선우) <burette@hanyang.ac.kr>
2019-03-16 20:50:19 +0900

    [MBS-15] [WIP] Implementing user controller.

--- api/src/main/java/com/moebius/api/controller/AccountController.java
+++ api/src/main/java/com/moebius/api/controller/AccountController.java
@@ -6,6 +6,9 @@ import com.moebius.backend.account.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -27,9 +30,11 @@ public class AccountController {
 	}
 
 	@PostMapping("/")
-	public Mono<AccountResponseDto> login(ServerWebExchange serverWebExchange) {
-
-		return null;
+	public Mono<UserDto> login(ServerWebExchange serverWebExchange) {
+		return ReactiveSecurityContextHolder.getContext()
+			.map(SecurityContext::getAuthentication)
+			.map(Authentication::getPrincipal)
+			.cast(UserDto.class); // TODO : Need to erase credentials and create session by serverWebExchange.
 	}
 
 	@PostMapping("/signup")
