f4e110258367814c82547abeff5797940590c4ab
knunu <burette@hanyang.ac.kr>
2019-04-01 01:19:52 +0900

    [MBS-15] Implemented webflux + security + thymeleaf finally.

--- frontend/src/main/java/com/moebius/frontend/FrontendContextLoader.java
+++ frontend/src/main/java/com/moebius/frontend/FrontendContextLoader.java
@@ -1,13 +1,22 @@
 package com.moebius.frontend;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.ViewResolverRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
 
 @Slf4j
 @Configuration
 @EnableWebFlux
+@RequiredArgsConstructor
 public class FrontendContextLoader implements WebFluxConfigurer {
+    private final ThymeleafReactiveViewResolver thymeleafReactiveViewResolver;
 
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(thymeleafReactiveViewResolver);
+    }
 }
--- frontend/src/main/java/com/moebius/frontend/api/MainController.java
+++ frontend/src/main/java/com/moebius/frontend/api/MainController.java
@@ -1,7 +1,6 @@
 package com.moebius.frontend.api;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index() {
         return "index";
     }
 }
