ddc2198eceeea169eaf4a697bf9bbeb0fdd3ba06
knunu <burette@hanyang.ac.kr>
2019-04-12 19:58:41 +0900

    [MBS-15] [WIP] Implementing jwt utils ...

--- backend/src/main/java/com/moebius/backend/configuration/security/JwtUtil.java
+++ backend/src/main/java/com/moebius/backend/configuration/security/JwtUtil.java
@@ -1,10 +1,20 @@
 package com.moebius.backend.configuration.security;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
 @Component
 public class JwtUtil implements Serializable {
+	private static final long serialVersionUID = 7286015171049934299L;
 
+	private static final String secret = "TEST_KEY";
+	private static final long expirationTime = 28800L;
+
+	Claims getAllClaimsFromToken(String token) {
+		return null;
+	}
+
+//	TODO : Implement JwtUtil for login process ...
 }
