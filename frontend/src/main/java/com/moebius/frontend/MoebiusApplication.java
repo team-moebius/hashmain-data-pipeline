338985f0fb8faa52932ad3f27284d49ea8107d37
knunu <burette@hanyang.ac.kr>
2019-04-27 22:48:30 +0900

    [MBS-0] [WIP] Need to fix dependency issue ...

--- frontend/src/main/java/com/moebius/frontend/MoebiusApplication.java
+++ frontend/src/main/java/com/moebius/frontend/MoebiusApplication.java
@@ -4,10 +4,10 @@ import com.moebius.backend.BackendContextLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackageClasses = { BackendContextLoader.class, FrontendContextLoader.class})
+@SpringBootApplication(scanBasePackageClasses = { BackendContextLoader.class, FrontendContextLoader.class })
 public class MoebiusApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MoebiusApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MoebiusApplication.class, args);
+	}
 }
