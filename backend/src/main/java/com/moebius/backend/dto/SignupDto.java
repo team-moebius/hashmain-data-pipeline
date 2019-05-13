8365fc889f120789887dfb63d48cf72db6eae957
knunu <burette@hanyang.ac.kr>
2019-05-13 23:38:38 +0900

    [MBS-15] Remove useless annotation related api and data.

--- backend/src/main/java/com/moebius/backend/dto/SignupDto.java
+++ backend/src/main/java/com/moebius/backend/dto/SignupDto.java
@@ -4,16 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class SignupDto {
-	@NotNull
 	private String name;
 	@Email
-	@NotNull
 	private String email;
-	@NotNull
 	private String password;
 }
