300a9f39cc89ed2c01154ceb63486548a8c39974
knunu <burette@hanyang.ac.kr>
2019-07-31 22:45:19 +0900

    [MBS-0] Added @CrossOrigin to stoploss controller.

--- backend/src/main/java/com/moebius/backend/api/StoplossController.java
+++ backend/src/main/java/com/moebius/backend/api/StoplossController.java
@@ -14,6 +14,7 @@ import reactor.core.publisher.Mono;
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/stoplosses")
 @RequiredArgsConstructor
