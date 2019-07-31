aa7701e9ca04d2ae1780c2712f0edfaf42cccaee
YoungGi Hong <topyk21@gamil.com>
2019-07-31 20:37:49 +0900

    Add alert components

--- frontend-react-typescript/src/App.tsx
+++ frontend-react-typescript/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  transitions,
+  positions,
+  Provider as AlertProvider,
+  AlertComponentPropsWithStyle,
+} from 'react-alert';
+
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import SignPage from 'pages/entry/SignPage';
 import MainPage from 'pages/entry/MainPage';
 import PrivateRoute from 'utils/PrviateRoute';
+import AlertContents from 'components/molecules/AlertContents';
 
 /** Material-ui theme setting */
 const defaultTheme = createMuiTheme({
@@ -24,12 +32,27 @@ const defaultTheme = createMuiTheme({
   },
 });
 
+/** Alert setting */
+const alertOptions = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 40000,
+  offset: '30px',
+  transition: transitions.FADE,
+};
+
+/** Alert template */
+const AlertTemplate = ({ style, options, message, close }: AlertComponentPropsWithStyle) => (
+  <div style={style}>
+    <AlertContents variant={options.type || 'info'} message={message} onClose={close} />
+  </div>
+);
+
 //TODO: connect with redux or mobx(signing props)
 const AppEntry = () => (
   <Router>
     <Switch>
       <Route component={SignPage} path="/sign" />
-      <PrivateRoute component={MainPage} path="/" redirectPath="/sign" signing={true} />
+      <PrivateRoute component={MainPage} path="/" redirectPath="/sign" signing={false} />
     </Switch>
   </Router>
 );
@@ -37,7 +60,9 @@ const AppEntry = () => (
 const App: React.FC = () => (
   <MuiThemeProvider theme={defaultTheme}>
     <CssBaseline>
-      <AppEntry />
+      <AlertProvider template={AlertTemplate} {...alertOptions}>
+        <AppEntry />
+      </AlertProvider>
     </CssBaseline>
   </MuiThemeProvider>
 );
