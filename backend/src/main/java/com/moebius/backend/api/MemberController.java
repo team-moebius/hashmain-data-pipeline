8b02430f4fcb5e25e9ff0e1a6e823f08e6b38ecc
knunu <burette@hanyang.ac.kr>
2019-08-03 01:04:52 +0900

    [MBS-0] remove the dot of end in message of swagger.

--- backend/src/main/java/com/moebius/backend/api/MemberController.java
+++ backend/src/main/java/com/moebius/backend/api/MemberController.java
@@ -32,10 +32,10 @@ public class MemberController {
 		notes = "성공할 경우 Json web token이 body에 담겨져 전달된다. 권한이 필요한 모든 요청의 Header에 'Authorization:Bearer ${JSON_WEB_TOKEN}'의 형태로 발송하면 된다."
 	)
 	@ApiResponses({
-		@ApiResponse(code = 200, message = "Success.", response = LoginResponseDto.class),
-		@ApiResponse(code = 400, message = "Password is wrong.", response = WrongDataException.class),
-		@ApiResponse(code = 401, message = "Email is not verified.", response = UnverifiedDataException.class),
-		@ApiResponse(code = 404, message = "Email is not found.", response = EmailNotFoundException.class),
+		@ApiResponse(code = 200, message = "Success", response = LoginResponseDto.class),
+		@ApiResponse(code = 400, message = "Password is wrong", response = WrongDataException.class),
+		@ApiResponse(code = 401, message = "Email is not verified", response = UnverifiedDataException.class),
+		@ApiResponse(code = 404, message = "Email is not found", response = EmailNotFoundException.class),
 	})
 	@PostMapping("")
 	public Mono<ResponseEntity<?>> login(@RequestBody @Valid @ApiParam(value = "로그인 시 필요한 정보", required = true) LoginDto loginDto) {
@@ -48,9 +48,9 @@ public class MemberController {
 		notes = "회원가입을 요청한다. 회원 가입 요청 후 인증 이메일도 함께 발송된다."
 	)
 	@ApiResponses({
-		@ApiResponse(code = 200, message = "Success.", response = String.class),
-		@ApiResponse(code = 400, message = "Requested email already exists.", response = VerificationFailedException.class),
-		@ApiResponse(code = 404, message = "Requested email is not found.", response = EmailNotFoundException.class),
+		@ApiResponse(code = 200, message = "Success", response = String.class),
+		@ApiResponse(code = 400, message = "Requested email already exists", response = VerificationFailedException.class),
+		@ApiResponse(code = 404, message = "Requested email is not found", response = EmailNotFoundException.class),
 	})
 	@PostMapping("/signup")
 	public Mono<ResponseEntity<?>> signup(@RequestBody @Valid @ApiParam(value = "회원가입 시 필요한 정보", required = true) SignupDto signupDto) {
@@ -72,8 +72,8 @@ public class MemberController {
 		notes = "회원 가입 시 중복된 이메일을 확인할 때 사용한다. 중복이면 Bad request(400), 중복이 아닐 경우 OK(200)을 반환한다."
 	)
 	@ApiResponses({
-		@ApiResponse(code = 200, message = "Success.", response = String.class),
-		@ApiResponse(code = 400, message = "Requested email already exists.", response = DuplicatedDataException.class),
+		@ApiResponse(code = 200, message = "Success", response = String.class),
+		@ApiResponse(code = 400, message = "Requested email already exists", response = DuplicatedDataException.class),
 	})
 	@GetMapping("/duplicated-email/{email}")
 	public Mono<ResponseEntity<String>> checkDuplicatedMember(@PathVariable String email) {
