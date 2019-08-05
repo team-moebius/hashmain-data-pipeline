b10c9a02f3def6e7f6e77d63b28f385778f15aca
knunu <burette@hanyang.ac.kr>
2019-08-05 23:42:50 +0900

    [MBS-0] Revert static resource handler for email header. & change host to dns from ip.

--- app/src/main/java/com/moebius/app/AppContextLoader.java
+++ app/src/main/java/com/moebius/app/AppContextLoader.java
@@ -6,15 +6,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ClientHttpConnector;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @ComponentScan(basePackageClasses = BackendContextLoader.class)
-public class AppContextLoader {
+public class AppContextLoader implements WebFluxConfigurer {
 	@Bean
 	public WebClient webClient() {
 		ClientHttpConnector connector = new ReactorClientHttpConnector();
 
 		return WebClient.builder().clientConnector(connector).build();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**")
+			.addResourceLocations("classpath:/static/");
 	}
 }
