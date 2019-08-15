465fc2afb45839cdc93dbdb029729421067a4921
knunu <burette@hanyang.ac.kr>
2019-08-16 00:31:14 +0900

    [MBS-0] update wrong static path of config.

--- backend/src/main/java/com/moebius/backend/service/tracker/TrackerService.java
+++ backend/src/main/java/com/moebius/backend/service/tracker/TrackerService.java
@@ -3,6 +3,7 @@ package com.moebius.backend.service.tracker;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moebius.backend.assembler.TradeAssembler;
 import com.moebius.backend.domain.commons.Exchange;
+import com.moebius.backend.domain.markets.MarketRepository;
 import com.moebius.backend.domain.trades.Trade;
 import com.moebius.backend.domain.trades.TradeRepository;
 import com.moebius.backend.dto.exchange.TradeDto;
@@ -23,18 +24,18 @@ import static com.moebius.backend.utils.ThreadScheduler.COMPUTE;
 import static com.moebius.backend.utils.ThreadScheduler.IO;
 
 /**
- * TODO : Extract this service as a independent project like moebius-tracker.
+ * TODO : Extract this service as a independent project as moebius-tracker.
  * This would be connected with message queue (kafka) or cache (redis) rather than db directly.
  */
 @Slf4j
 @Service
-@Profile("tracker")
+@Profile("local")
 @RequiredArgsConstructor
-public class TrackerService implements ApplicationListener<ApplicationReadyEvent> {
+public class UpbitTrackerService implements ApplicationListener<ApplicationReadyEvent> {
 	private final WebSocketClient webSocketClient;
 	private final TradeRepository tradeRepository;
+	private final MarketRepository marketRepository;
 	private final TradeAssembler tradeAssembler;
-	private final String message = "[{\"ticket\":\"moebius-test\"},{\"type\":\"trade\",\"codes\":[\"KRW-BTC\",\"KRW-BCH\",\"KRW-XRP\",\"KRW-EOS\",\"KRW-ETH\"]},{\"format\":\"SIMPLE\"}]";
 	@Value("${exchange.upbit.websocket.uri}")
 	private String uri;
 
@@ -44,26 +45,35 @@ public class TrackerService implements ApplicationListener<ApplicationReadyEvent
 	}
 
 	private void trackTrades() {
-		log.info("[Tracker] Start to track trades. - message : {}", message);
-		webSocketClient.execute(URI.create(uri),
-			session -> session.send(Mono.just(session.textMessage(message)))
-				.thenMany(session.receive().map(webSocketMessage -> {
-					ObjectMapper objectMapper = new ObjectMapper();
+		ObjectMapper objectMapper = new ObjectMapper();
 
-					try {
-						TradeDto tradeDto = objectMapper.readValue(webSocketMessage.getPayloadAsText(), TradeDto.class);
-						tradeDto.setExchange(Exchange.UPBIT);
-						accumulateTrade(tradeDto);
-						// maybe need to use upsertTrade rather accumulateTrade.
-						// upsertTrade(tradeDto);
-					} catch (IOException e) {
-						log.error(e.getMessage());
-					}
-					return webSocketMessage;
-				}))
-				.then())
-			.doOnSuccess(aVoid -> trackTrades())
-			.subscribe();
+		marketRepository.findAllByExchangeAndActive(Exchange.UPBIT, true)
+			.subscribeOn(IO.scheduler())
+			.publishOn(COMPUTE.scheduler())
+			.map(market -> "\"" + market.getSymbol().toString() + "\"")
+			.reduce((prevSymbol, nextSymbol) -> prevSymbol + "," + nextSymbol)
+			.map(rawMessage -> "[{\"ticket\":\"moebius-tracker\"},{\"type\":\"trade\",\"codes\":[" + rawMessage + "]},{\"format\":\"SIMPLE\"}]")
+			.map(message -> {
+					log.info("[Tracker] Start to track trades. - message : {}", message);
+					return webSocketClient.execute(URI.create(uri),
+						session -> session.send(Mono.just(session.textMessage(message)))
+								.thenMany(session.receive().map(webSocketMessage -> {
+									try {
+										TradeDto tradeDto = objectMapper.readValue(webSocketMessage.getPayloadAsText(), TradeDto.class);
+										tradeDto.setExchange(Exchange.UPBIT);
+										//									accumulateTrade(tradeDto);
+										// maybe need to use upsertTrade rather accumulateTrade.
+										// upsertTrade(tradeDto);
+									} catch (IOException e) {
+										log.error(e.getMessage());
+									}
+									return webSocketMessage;
+								}))
+								.then())
+						.doOnTerminate(this::trackTrades)
+						.subscribe();
+				}
+			).subscribe();
 	}
 
 	private void accumulateTrade(TradeDto tradeDto) {
