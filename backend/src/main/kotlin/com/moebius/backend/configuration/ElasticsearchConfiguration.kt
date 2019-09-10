396c9d65018101cf0b3621cd9a48e599184a08e1
just-kim <47264873+just-kim@users.noreply.github.com>
2019-09-11 00:51:00 +0900

    #78 Add elasticsearch authentication (#79)
    - Add elasticsearch authentication
    - Add elasticsearch configuration logging
    - Add config for elasticsearch authentication

--- backend/src/main/kotlin/com/moebius/backend/configuration/ElasticsearchConfiguration.kt
+++ backend/src/main/kotlin/com/moebius/backend/configuration/ElasticsearchConfiguration.kt
@@ -1,8 +1,13 @@
 package com.moebius.backend.configuration
 
 
+import mu.KotlinLogging
 import org.apache.http.HttpHost
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.impl.client.BasicCredentialsProvider
 import org.elasticsearch.client.RestClient
+import org.elasticsearch.client.RestClientBuilder
 import org.elasticsearch.client.RestHighLevelClient
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -16,9 +21,27 @@ class ElasticsearchConfiguration(
         var elasticsearchProperties: ElasticsearchProperties
 ) {
 
+    private val log = KotlinLogging.logger {}
+
     @Bean
-    fun restHighLevelClient(): RestHighLevelClient {
-        return RestHighLevelClient(RestClient.builder(HttpHost.create(elasticsearchProperties.clusterNodes)))
+    fun restClientBuilder(): RestClientBuilder {
+        val userName = elasticsearchProperties.properties["username"]
+        val password = elasticsearchProperties.properties["password"]
+        val builder = RestClient.builder(HttpHost.create(elasticsearchProperties.clusterNodes))
+        log.info("elasticsearch configuration: username:$userName / password:$password / cluster-nodes:${elasticsearchProperties.clusterNodes}")
+        if (userName != null && password != null) {
+            val credentialsProvider = BasicCredentialsProvider()
+            credentialsProvider.setCredentials(AuthScope.ANY, UsernamePasswordCredentials(userName, password))
+            builder.setHttpClientConfigCallback {
+                it.setDefaultCredentialsProvider(credentialsProvider)
+            }
+        }
+        return builder
+    }
+
+    @Bean
+    fun restHighLevelClient(restClientBuilder: RestClientBuilder): RestHighLevelClient {
+        return RestHighLevelClient(restClientBuilder)
     }
 
 }
