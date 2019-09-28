8dc6154b194de824c6d7cf60c2716bccd7240d57
just.kim <just.kim.dev@gmail.com>
2019-09-28 14:22:09 +0900

    Fix producer and consumer initializer for cycle runtime error

--- backend/src/main/java/com/moebius/backend/service/kafka/trade/TradeKafkaConsumer.java
+++ backend/src/main/java/com/moebius/backend/service/kafka/trade/TradeKafkaConsumer.java
@@ -4,6 +4,7 @@ import com.moebius.backend.domain.trades.TradeDocument;
 import com.moebius.backend.service.kafka.KafkaConsumer;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.stereotype.Component;
 import reactor.core.Disposable;
@@ -16,8 +17,8 @@ import java.util.Map;
 @Component
 public class TradeKafkaConsumer extends KafkaConsumer<String, TradeDocument> {
 	private static final String TRADE_KAFKA_TOPIC = "moebius.trade.upbit";
-
-	public TradeKafkaConsumer(Map<String, Object> receiverDefaultProperties) {
+	
+	public TradeKafkaConsumer(@Qualifier("receiverDefaultProperties") Map<String, Object> receiverDefaultProperties) {
 		super(receiverDefaultProperties);
 	}
 
