7b3877fce75a7bcb8a05e5bfb205ccf0f8252b8a
knunu <burette@hanyang.ac.kr>
2019-09-29 17:31:07 +0900

    [MBS-0] Fix wrong query of findAllByApiKeyId.

--- backend/src/main/java/com/moebius/backend/domain/orders/OrderRepository.java
+++ backend/src/main/java/com/moebius/backend/domain/orders/OrderRepository.java
@@ -1,13 +1,11 @@
 package com.moebius.backend.domain.orders;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 @Repository
 public interface OrderRepository extends ReactiveMongoRepository<Order, ObjectId> {
-	@Query(value = "{ 'apiKey.id' : ?0 }")
 	Flux<Order> findAllByApiKeyId(ObjectId apiKeyId);
 }
