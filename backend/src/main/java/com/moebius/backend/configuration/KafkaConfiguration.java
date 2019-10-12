c4c2defc3f924d523da734576dca1b265228727c
knunu <burette@hanyang.ac.kr>
2019-10-13 00:39:46 +0900

    [MBS-68] - Added NoArgsConstructor(Default Constructor) to TradeDocument for deserializing. - Binded consuming process to KafkaConsumer constructor (Auto consume). - Added trade package to trust packages. - Remove useless KafkaTrigger.

--- backend/src/main/java/com/moebius/backend/configuration/KafkaConfiguration.java
+++ backend/src/main/java/com/moebius/backend/configuration/KafkaConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,11 +39,11 @@ public class KafkaConfiguration {
 	@Bean
 	public Map<String, String> receiverDefaultProperties() {
 		Map<String, String> receiverDefaultProperties = new HashMap<>();
-
 		receiverDefaultProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, String.join(",", kafkaProperties.getBootstrapServers()));
 		receiverDefaultProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, kafkaProperties.getConsumer().getClientId());
 		receiverDefaultProperties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getConsumer().getGroupId());
 		receiverDefaultProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaProperties.getConsumer().getAutoOffsetReset());
+		receiverDefaultProperties.put(JsonDeserializer.TRUSTED_PACKAGES, "com.moebius.backend.domain.trades");
 		receiverDefaultProperties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SECURITY_PROTOCOL);
 		receiverDefaultProperties.put(SaslConfigs.SASL_MECHANISM, SASL_MECHANISM);
 		receiverDefaultProperties.put(SaslConfigs.SASL_JAAS_CONFIG, getJaasConfig());
--- backend/src/main/java/com/moebius/backend/service/kafka/KafkaConsumer.java
+++ backend/src/main/java/com/moebius/backend/service/kafka/KafkaConsumer.java
@@ -2,7 +2,6 @@ package com.moebius.backend.service.kafka;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import reactor.core.Disposable;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.receiver.ReceiverRecord;
@@ -15,8 +14,6 @@ import static com.moebius.backend.utils.ThreadScheduler.KAFKA;
 
 @Slf4j
 public abstract class KafkaConsumer<K, V> {
-	private final KafkaReceiver<K, V> receiver;
-
 	public KafkaConsumer(Map<String, String> receiverDefaultProperties) {
 		Map<String, Object> properties = new HashMap<>(receiverDefaultProperties);
 		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, getKeyDeserializerClass());
@@ -28,7 +25,9 @@ public abstract class KafkaConsumer<K, V> {
 			.addAssignListener(partitions -> log.debug("[Kafka] onPartitionsAssigned {}", partitions))
 			.addRevokeListener(partitions -> log.debug("[Kafka] onPartitionsRevoked {}", partitions));
 
-		receiver = KafkaReceiver.create(receiverOptions);
+		KafkaReceiver<K, V> receiver = KafkaReceiver.create(receiverOptions);
+		log.info("[Kafka] Start to read messages. [{}]", getTopic());
+		receiver.receive().subscribe(this::processRecord);
 	}
 
 	public abstract String getTopic();
@@ -38,9 +37,4 @@ public abstract class KafkaConsumer<K, V> {
 	protected abstract Class<?> getKeyDeserializerClass();
 
 	protected abstract Class<?> getValueDeserializerClass();
-
-	public void consumeMessages() {
-		log.info("[Kafka] Start to read messages. [{}]", getTopic());
-		Disposable disposable = receiver.receive().subscribe(this::processRecord);
-	}
 }
