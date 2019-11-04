bd6380f83c8b5cb93697e6d212210920bca85fe3
knunu <burette@hanyang.ac.kr>
2019-11-04 23:56:43 +0900

    [MBS-99] [WIP] Added OrderStatus and Fix index.

--- backend/src/main/java/com/moebius/backend/domain/orders/Order.java
+++ backend/src/main/java/com/moebius/backend/domain/orders/Order.java
@@ -14,7 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @ToString(exclude = "apiKeyId")
 @Document(collection = "orders")
-@CompoundIndex(def = "{'exchange': 1, 'symbol': 1, 'orderPosition': 1}")
+@CompoundIndex(def = "{'price': 1, 'symbol': 1, 'orderPosition': 1, 'orderStatus': 1, 'orderType': 1, 'exchange': 1}")
 public class Order extends Base {
     @Id
     private ObjectId id;
@@ -23,6 +23,7 @@ public class Order extends Base {
     // Symbol has been changed to String from Enum cause of real time changes in external exchanges.
     private String symbol;
     private OrderPosition orderPosition;
+    private OrderStatus orderStatus;
     private OrderType orderType;
     private double price;
     private double volume;
