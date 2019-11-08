0cbbe48c95f4b8ded4e4161c9f094704b479002d
knunu <burette@hanyang.ac.kr>
2019-11-08 17:40:51 +0900

    [MBS-99] [WIP] Refactoring OrderRepository ...

--- backend/src/main/java/com/moebius/backend/domain/orders/OrderRepositoryImpl.java
+++ backend/src/main/java/com/moebius/backend/domain/orders/OrderRepositoryImpl.java
@@ -3,8 +3,11 @@ package com.moebius.backend.domain.orders;
 import com.moebius.backend.domain.commons.Exchange;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,11 +16,27 @@ public class OrderRepositoryImpl implements OrderRepositoryCustom {
 
 	@Override
 	public Flux<Order> findAndUpdateAllByAskCondition(Exchange exchange, String symbol, OrderPosition orderPosition, double price) {
-		return null;
+		Query query = new Query(Criteria.where("exchange").is(exchange)
+			.where("symbol").is(symbol)
+			.where("orderPosition").is(orderPosition)
+			.where("price").gte(price));
+
+		// TODO : If needed, use inTransaction like this
+//		mongoTemplate.inTransaction()
+//			.execute(operations -> operations.find())
+//			.thenMany()
+
+		return mongoTemplate.find(query, Order.class)
+			.flatMap(this::updateOrderStatusToExecuted);
 	}
 
 	@Override
 	public Flux<Order> findAndUpdateAllByBidCondition(Exchange exchange, String symbol, OrderPosition orderPosition, double price) {
 		return null;
+	}
+
+	private Mono<Order> updateOrderStatusToExecuted(Order order) {
+		order.setOrderStatus(OrderStatus.EXECUTED);
+		return mongoTemplate.save(order);
 	}
 }
