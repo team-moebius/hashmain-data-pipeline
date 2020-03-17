54ae1512a35f9dd66c8340a1e0ca03ac501c26ba
knunu <burette@hanyang.ac.kr>
2020-03-17 23:51:25 +0900

    [MBS-119] Initialize market public api.

--- backend/src/main/java/com/moebius/backend/api/MarketController.java
+++ backend/src/main/java/com/moebius/backend/api/MarketController.java
@@ -2,9 +2,11 @@ package com.moebius.backend.api;
 
 import com.moebius.backend.domain.commons.Exchange;
 import com.moebius.backend.dto.MarketDto;
+import com.moebius.backend.dto.frontend.response.MarketResponseDto;
 import com.moebius.backend.service.market.MarketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -17,17 +19,26 @@ public class MarketController {
 	private final MarketService marketService;
 
 	@PostMapping("")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public Mono<ResponseEntity<String>> createMarket(@RequestBody @Valid MarketDto marketDto) {
 		return marketService.createMarket(marketDto);
 	}
 
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public Mono<ResponseEntity<String>> deleteMarket(@PathVariable String id) {
 		return marketService.deleteMarket(id);
 	}
 
-	@PutMapping("/exchanges/{exchange}")
+	@PutMapping("/{exchange}")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public Mono<ResponseEntity<?>> updateMarketsByExchange(@PathVariable Exchange exchange) {
-		return marketService.updateMarketsByExchange(exchange);
+		return marketService.updateMarketsByExchange(Exchange.getBy(exchange.toString()));
+	}
+
+	@GetMapping("/{exchange}")
+	@PreAuthorize("hasAuthority('MEMBER')")
+	public Mono<ResponseEntity<MarketResponseDto>> getMarketsByExchange(@PathVariable String exchange) {
+		return null;
 	}
 }
