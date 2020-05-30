1035a07eb112c7d536c6afda1ea67bc0624f6baa
knunu <burette@hanyang.ac.kr>
2020-05-30 22:51:52 +0900

    [MBS-44] Implementing upbit service test ...

--- backend/src/test/groovy/com/moebius/backend/service/exchange/UpbitServiceTest.groovy
+++ backend/src/test/groovy/com/moebius/backend/service/exchange/UpbitServiceTest.groovy
@@ -1,28 +1,39 @@
 package com.moebius.backend.service.exchange
 
 import com.moebius.backend.assembler.exchange.UpbitAssembler
+import com.moebius.backend.domain.commons.Exchange
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.test.StepVerifier
 import spock.lang.Specification
 import spock.lang.Subject
 
 class UpbitServiceTest extends Specification {
 	def webClient = Mock(WebClient)
 	def upbitAssembler = Mock(UpbitAssembler)
+	def accessKey = "accessKey"
+	def secretKey = "secretKey"
 
 	@Subject
-	def upbitService = new UpbitService()
+	def upbitService = new UpbitService(webClient, upbitAssembler)
 
-	def "GetExchange"() {
-
+	def "Should get exchange type of upbit"() {
+		expect:
+		upbitService.getExchange() == Exchange.UPBIT
 	}
 
-	def "GetAuthToken"() {
+	def "Should get auth token"() {
+		expect:
+		StepVerifier.create(upbitService.getAuthToken(accessKey, secretKey))
+				.assertNext({
+					it != null
+					it.length() > 0
+				}).verifyComplete()
 	}
 
-	def "GetAssets"() {
+	def "Should get assets"() {
 	}
 
-	def "CheckHealth"() {
+	def "Should check health"() {
 	}
 
 	def "RequestOrder"() {
