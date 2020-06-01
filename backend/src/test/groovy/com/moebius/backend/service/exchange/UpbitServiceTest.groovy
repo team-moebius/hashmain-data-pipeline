4b58d3ea3fc93a9145b98458569efda17b69f8b2
knunu <burette@hanyang.ac.kr>
2020-06-01 10:26:39 +0900

    [MBS-44] Update unit test code for getting assets.

--- backend/src/test/groovy/com/moebius/backend/service/exchange/UpbitServiceTest.groovy
+++ backend/src/test/groovy/com/moebius/backend/service/exchange/UpbitServiceTest.groovy
@@ -2,16 +2,26 @@ package com.moebius.backend.service.exchange
 
 import com.moebius.backend.assembler.exchange.UpbitAssembler
 import com.moebius.backend.domain.commons.Exchange
+import com.moebius.backend.dto.exchange.upbit.UpbitAssetDto
+import org.springframework.http.HttpHeaders
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
 import reactor.test.StepVerifier
 import spock.lang.Specification
 import spock.lang.Subject
 
+import java.util.function.Consumer
+
 class UpbitServiceTest extends Specification {
 	def webClient = Mock(WebClient)
 	def upbitAssembler = Mock(UpbitAssembler)
+	def uriSpec = Mock(WebClient.RequestHeadersUriSpec)
+	def headersSpec = Mock(WebClient.RequestHeadersSpec)
+	def responseSpec = Mock(WebClient.ResponseSpec)
+
 	def accessKey = "accessKey"
 	def secretKey = "secretKey"
+	def authToken = "authToken"
 
 	@Subject
 	def upbitService = new UpbitService(webClient, upbitAssembler)
@@ -31,8 +41,25 @@ class UpbitServiceTest extends Specification {
 	}
 
 	def "Should get assets"() {
+		given:
+		1 * webClient.get() >> uriSpec
+		1 * uriSpec.uri(_ as String) >> headersSpec
+		1 * headersSpec.headers(_ as Consumer<HttpHeaders>) >> headersSpec
+		1 * headersSpec.retrieve() >> responseSpec
+		1 * responseSpec.bodyToFlux(UpbitAssetDto.class) >> Flux.just(UpbitAssetDto.builder()
+				.currency("BTC")
+				.balance("10000")
+				.averagePurchasePrice("2000000")
+				.build())
+
 		expect:
-		StepVerifier.create(upbitService.getAssets())
+		StepVerifier.create(upbitService.getAssets(authToken))
+				.assertNext({
+					it != null
+					it.getCurrency() == "BTC"
+					it.getBalance() == 10000D
+					it.getAveragePurchasePrice() == 2000000D
+				}).verifyComplete()
 	}
 
 	def "Should check health"() {
