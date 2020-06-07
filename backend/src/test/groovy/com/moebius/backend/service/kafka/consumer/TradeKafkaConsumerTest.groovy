8700e2778fafe297af386dd52d458e16972a2cc2
knunu <burette@hanyang.ac.kr>
2020-06-07 23:59:39 +0900

    [MBS-44] [WIP] Implementing test codes for trade kafka consumer.

--- backend/src/test/groovy/com/moebius/backend/service/kafka/consumer/TradeKafkaConsumerTest.groovy
+++ backend/src/test/groovy/com/moebius/backend/service/kafka/consumer/TradeKafkaConsumerTest.groovy
@@ -1,9 +1,35 @@
 package com.moebius.backend.service.kafka.consumer
 
+import reactor.core.publisher.Flux
+import reactor.kafka.receiver.KafkaReceiver
+import reactor.kafka.receiver.ReceiverRecord
 import spock.lang.Specification
+import spock.lang.Subject
+
+import java.util.function.Consumer
+
+import static com.moebius.backend.utils.ThreadScheduler.COMPUTE
 
 class TradeKafkaConsumerTest extends Specification {
-	def "ConsumeMessages"() {
+	def receiver = Mock(KafkaReceiver)
+	def fluxWithReceiverRecord = Flux.just(Stub(ReceiverRecord))
+	def consumer = Stub(Consumer)
+
+	@Subject
+	def tradeKafkaConsumer = new TradeKafkaConsumer([:])
+
+	def "Should consume messages"() {
+		given:
+		1 * receiver.receive() >> fluxWithReceiverRecord
+		1 * fluxWithReceiverRecord.publishOn(COMPUTE.scheduler())
+		1 * fluxWithReceiverRecord.subscribe()
+
+		when:
+		tradeKafkaConsumer.consumeMessages()
+
+		then:
+		1 * receiver.receive() >> Flux.just(receiverRecord)
+		1 * receiverRecord
 	}
 
 	def "GetTopic"() {
