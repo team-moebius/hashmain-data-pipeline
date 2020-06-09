4aae550b98cf9f18e4af08a63d1cd98ef0974046
knunu <burette@hanyang.ac.kr>
2020-06-10 00:37:47 +0900

    [MBS-44] [WIP] Implementing test codes for trade kafka consumer.

--- backend/src/test/groovy/com/moebius/backend/service/kafka/consumer/TradeKafkaConsumerTest.groovy
+++ backend/src/test/groovy/com/moebius/backend/service/kafka/consumer/TradeKafkaConsumerTest.groovy
@@ -1,36 +1,26 @@
 package com.moebius.backend.service.kafka.consumer
 
-
+import com.moebius.backend.dto.TradeDto
 import com.moebius.backend.service.market.MarketService
 import com.moebius.backend.service.order.ExchangeOrderService
-import reactor.core.publisher.Flux
-import reactor.kafka.receiver.KafkaReceiver
 import reactor.kafka.receiver.ReceiverRecord
-import reactor.test.StepVerifier
 import spock.lang.Specification
 import spock.lang.Subject
 
-import java.util.function.Consumer
-
-import static com.moebius.backend.utils.ThreadScheduler.COMPUTE
-
 class TradeKafkaConsumerTest extends Specification {
-	def receiver = Mock(KafkaReceiver)
-	def fluxWithReceiverRecord = Flux.just(Stub(ReceiverRecord))
-	def consumer = Stub(Consumer)
+	def receiverRecord = Stub(ReceiverRecord) {
+		value() >> Stub(TradeDto)
+	}
 
 	@Subject
 	def tradeKafkaConsumer = new TradeKafkaConsumer([:], Stub(ExchangeOrderService), Stub(MarketService))
 
 	def "Should consume messages"() {
-		given:
-		1 * receiver.receive() >> fluxWithReceiverRecord
-		1 * fluxWithReceiverRecord.publishOn(COMPUTE.scheduler()) >> fluxWithReceiverRecord
-		1 * fluxWithReceiverRecord.subscribe(consumer)
+		when:
+		tradeKafkaConsumer.consumeMessages()
 
-		expect:
-		StepVerifier.create(tradeKafkaConsumer.consumeMessages())
-				.expectSubscription()
+		then:
+		tradeKafkaConsumer.processRecord(receiverRecord)
 	}
 
 	def "Should get topic"() {
