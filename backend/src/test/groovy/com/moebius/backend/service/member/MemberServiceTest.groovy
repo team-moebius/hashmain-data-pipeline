19330e51418e2c997a993609eb7df1f1aa4c2481
knunu <burette@hanyang.ac.kr>
2020-06-23 19:57:00 +0900

    [MBS-44] [WIP] Implementing member service test.

--- backend/src/test/groovy/com/moebius/backend/service/member/MemberServiceTest.groovy
+++ backend/src/test/groovy/com/moebius/backend/service/member/MemberServiceTest.groovy
@@ -3,7 +3,20 @@ package com.moebius.backend.service.member
 import com.moebius.backend.assembler.MemberAssembler
 import com.moebius.backend.domain.members.Member
 import com.moebius.backend.domain.members.MemberRepository
+import com.moebius.backend.dto.frontend.LoginDto
+import com.moebius.backend.dto.frontend.MemberDto
+import com.moebius.backend.dto.frontend.SignupDto
+import com.moebius.backend.exception.DataNotFoundException
+import com.moebius.backend.exception.DataNotVerifiedException
+import com.moebius.backend.exception.DuplicatedDataException
+import com.moebius.backend.exception.WrongDataException
+import com.mongodb.DuplicateKeyException
+import com.mongodb.ServerAddress
+import com.mongodb.WriteConcernResult
+import org.bson.BsonDocument
+import org.bson.types.ObjectId
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.crypto.password.PasswordEncoder
 import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
@@ -36,6 +49,7 @@ class MemberServiceTest extends Specification {
 					it.getStatusCode() == HttpStatus.OK
 					it.getBody() == email
 				})
+				.verifyComplete()
 	}
 
 	def "Should check non-duplicated member"() {
@@ -48,15 +62,96 @@ class MemberServiceTest extends Specification {
 					it != null
 					it.getStatusCode() == HttpStatus.OK
 				})
+				.verifyComplete()
 	}
 
 	def "Should create member"() {
+		given:
+		1 * memberAssembler.toMember(_ as SignupDto) >> Stub(Member)
+		1 * memberRepository.save(_ as Member) >> Mono.just(Stub(Member))
+		1 * emailService.requestToVerifyEmail(_ as String) >> Mono.just(ResponseEntity.ok(HttpStatus.OK.getReasonPhrase()))
 
+		expect:
+		StepVerifier.create(memberService.createMember(Stub(SignupDto)))
+				.assertNext({
+					it != null
+					it.getStatusCode() == HttpStatus.OK
+				})
+				.verifyComplete()
 	}
 
-	def "Login"() {
+	def "Should not create duplicated member"() {
+		given:
+		1 * memberAssembler.toMember(_ as SignupDto) >> Stub(Member)
+		1 * memberRepository.save(_ as Member) >> { throw(new DuplicateKeyException(Stub(BsonDocument), Stub(ServerAddress), Stub(WriteConcernResult))) }
+
+		expect:
+		StepVerifier.create(memberService.createMember(Stub(SignupDto)))
+				.verifyError(DuplicatedDataException.class)
+	}
+
+	def "Should login"() {
+		given:
+		def member = Stub(Member) {
+			isActive() >> true
+		}
+
+		1 * memberRepository.findByEmail(_ as String) >> Mono.just(member)
+		1 * passwordEncoder.matches(_ as CharSequence, _ as String) >> true
+
+		expect:
+		StepVerifier.create(memberService.login(Stub(LoginDto)))
+				.assertNext({
+					it != null
+					it.getStatusCode() == HttpStatus.OK
+				})
+				.verifyComplete()
+	}
+
+	def "Should not login because of not existent email"() {
+		given:
+		1 * memberRepository.findByEmail(_ as String) >> Mono.empty()
+
+		expect:
+		StepVerifier.create(memberService.login(Stub(LoginDto)))
+				.verifyError(DataNotFoundException.class)
+	}
+
+	def "Should not login because of wrong password"() {
+		given:
+		1 * memberRepository.findByEmail(_ as String) >> Mono.just(Stub(Member))
+		1 * passwordEncoder.matches(_ as CharSequence, _ as String) >> false
+
+		expect:
+		StepVerifier.create(memberService.login(Stub(LoginDto)))
+				.verifyError(WrongDataException.class)
+	}
+
+	def "Should not login because of unactivated member"() {
+		given:
+		def member = Stub(Member) {
+			isActive() >> false
+		}
+
+		1 * memberRepository.findByEmail(_ as String) >> Mono.just(member)
+		1 * passwordEncoder.matches(_ as CharSequence, _ as String) >> true
+
+		expect:
+		StepVerifier.create(memberService.login(Stub(LoginDto)))
+				.verifyError(DataNotVerifiedException.class)
 	}
 
 	def "Should get member"() {
+		given:
+		1 * memberRepository.findById(_ as ObjectId) >> Mono.just(Stub(Member))
+		1 * memberAssembler.toDto(_ as Member) >> Stub(MemberDto)
+
+		expect:
+		StepVerifier.create(memberService.getMember("5e52ab07b6e75567ac2376ae"))
+				.assertNext({
+					it != null
+					it.getStatusCode() == HttpStatus.OK
+				})
+				.verifyComplete()
 	}
 }
