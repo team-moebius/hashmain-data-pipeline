39fcff0b6547d1a12e34c63a76ed88681e33c45a
knunu <burette@hanyang.ac.kr>
2020-06-30 23:10:06 +0900

    [MBS-44] [WIP] Implementing integration tests ...

---
+++ backend/src/test/groovy/com/moebius/backend/api/MarketControllerIntegrationTest.groovy
@@ -0,0 +1,14 @@
+package com.moebius.backend.api
+
+import spock.lang.Specification
+
+class MarketControllerIntegrationTest extends Specification {
+	def "DeleteMarket"() {
+	}
+
+	def "UpdateMarketsByExchange"() {
+	}
+
+	def "GetMarketsByExchange"() {
+	}
+}
---
+++ backend/src/test/groovy/com/moebius/backend/api/MemberControllerIntegrationTest.groovy
@@ -0,0 +1,23 @@
+package com.moebius.backend.api
+
+import spock.lang.Specification
+
+class MemberControllerIntegrationTest extends Specification {
+	def "Login"() {
+	}
+
+	def "GetMember"() {
+	}
+
+	def "Signup"() {
+	}
+
+	def "FindPassword"() {
+	}
+
+	def "CheckDuplicateMember"() {
+	}
+
+	def "VerifyEmail"() {
+	}
+}
