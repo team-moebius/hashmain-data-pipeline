d8fa18c3d0bf4f9f20a346c437553d689c326b61
knunu <burette@hanyang.ac.kr>
2020-07-19 23:56:49 +0900

    [MBS-44] [WIP] Implementing upbit assembler test.

--- backend/src/test/groovy/com/moebius/backend/assembler/exchange/UpbitAssemblerTest.groovy
+++ backend/src/test/groovy/com/moebius/backend/assembler/exchange/UpbitAssemblerTest.groovy
@@ -1,14 +1,43 @@
 package com.moebius.backend.assembler.exchange
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.moebius.backend.domain.orders.Order
+import com.moebius.backend.dto.OrderStatusDto
+import com.moebius.backend.dto.exchange.upbit.UpbitOrderDto
+import com.moebius.backend.dto.exchange.upbit.UpbitOrderStatusDto
+import org.bson.types.ObjectId
 import spock.lang.Specification
+import spock.lang.Subject
 
 class UpbitAssemblerTest extends Specification {
-	def "ToOrderDto"() {
+	def orderId = "5eb6df8abd5d1a46cfcef11b"
+	def order = Stub(Order) {
+		getId() >> new ObjectId("5eb6df8abd5d1a46cfcef11b")
+	}
+	def objectMapper = Mock(ObjectMapper)
+
+	@Subject
+	def upbitAssembler = new UpbitAssembler(objectMapper)
+
+	def "Should assemble order"() {
+		when:
+		def result = upbitAssembler.assembleOrder(order)
+
+		then:
+		result instanceof UpbitOrderDto
+		result.getIdentifier() == orderId
 	}
 
-	def "ToOrderStatusDto"() {
+	def "Should assemble order status"() {
+		when:
+		def result = upbitAssembler.assembleOrderStatus(orderId, Stub(UpbitOrderStatusDto))
+
+		then:
+		result instanceof OrderStatusDto
+		result.getId() == orderId
 	}
 
 	def "AssembleOrderParameters"() {
+
 	}
 }
