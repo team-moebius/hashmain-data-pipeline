8e787613df96f9591130c0a325c1ecb2b2b847a4
pointware <pwangkk@gmail.com>
2020-09-09 00:00:17 +0900

    add trade aggs empty result

--- api/src/test/groovy/com/moebius/api/service/TradeAggregationServiceTest.groovy
+++ api/src/test/groovy/com/moebius/api/service/TradeAggregationServiceTest.groovy
@@ -1,21 +1,44 @@
 package com.moebius.api.service
 
+import com.moebius.api.dto.TradeAggregationRequest
 import com.moebius.data.type.Exchange
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import spock.lang.Ignore
+import org.springframework.test.context.TestPropertySource
 import spock.lang.Specification
 
-@SpringBootTest
+@SpringBootTest()
+@TestPropertySource(properties = [
+    "spring.elasticsearch.rest.username=moebius",
+    "spring.elasticsearch.rest.password=highbal1!"
+]
+)
 class TradeAggregationServiceTest extends Specification {
 
     @Autowired
     TradeAggregationService tradeAggregationService;
 
-    @Ignore
     def "GetTradeAggregation"() {
         when:
-        def result = tradeAggregationService.getTradeAggregation(Exchange.UPBIT, "KRW-SC", 10000)
+        def request = TradeAggregationRequest.builder()
+        .exchange(Exchange.UPBIT)
+        .symbol("KRW-SC")
+        .minutesAgo(5)
+                .build()
+        def result = tradeAggregationService.getTradeAggregation(request)
+
+        then:
+        result != null
+    }
+
+    def "GetTradeAggregation_isEmpty"() {
+        when:
+        def request = TradeAggregationRequest.builder()
+                .exchange(Exchange.UPBIT)
+                .symbol("-")
+                .minutesAgo(5)
+                .build()
+        def result = tradeAggregationService.getTradeAggregation(request)
 
         then:
         result != null
