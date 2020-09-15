f01fa4dbc66ef0802165d8b796e557d941a784a7
pointware <pwangkk@gmail.com>
2020-09-16 01:10:41 +0900

    refactoring get timekey

--- api/src/main/java/com/moebius/api/mapper/TradeStatsAggregationBucketMapper.java
+++ api/src/main/java/com/moebius/api/mapper/TradeStatsAggregationBucketMapper.java
@@ -5,14 +5,16 @@ import com.moebius.api.dto.TradeStatsAggregationBucketDto;
 import com.moebius.api.entity.TradeStatsAggregation;
 import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
+
 @Component
 public class TradeStatsAggregationBucketMapper {
     public TradeStatsAggregationBucketDto map(TradeStatsAggregation aggregation, TradeAggregationRequest request) {
         final var zoneId = request.getFrom().getZone();
+        ZonedDateTime zonedDateTime = aggregation.getTimeKey().withZoneSameInstant(zoneId);
         return TradeStatsAggregationBucketDto.builder()
-                .startTime(aggregation.getTimeKey().withZoneSameInstant(zoneId))
-                .endTime(aggregation.getTimeKey().withZoneSameInstant(zoneId)
-                        .plusMinutes(request.getInterval()))
+                .startTime(zonedDateTime)
+                .endTime(zonedDateTime.plusMinutes(request.getInterval()))
                 .totalAskCount(Double.valueOf(aggregation.getTotalAskCount()).longValue())
                 .totalBidCount(Double.valueOf(aggregation.getTotalBidCount()).longValue())
                 .totalTransactionCount(Double.valueOf(aggregation.getTotalTransactionCount()).longValue())
