a5fafb6fed6669f47d84bb4c1170176f061a36ba
pointware <pwangkk@gmail.com>
2020-12-06 19:51:10 +0900

    add actuator health checker

---
+++ tracker/src/main/kotlin/com/moebius/tracker/actuator/TrackerHealthCheckIndicator.kt
@@ -0,0 +1,36 @@
+package com.moebius.tracker.actuator
+
+import com.moebius.backend.service.kafka.producer.TradeKafkaProducer
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.util.function.Tuple2
+
+@Component("tracker")
+class TrackerHealthCheckIndicator(
+        private val tradeKafkaProducer: TradeKafkaProducer,
+        meterRegistry: MeterRegistry
+) : ReactiveHealthIndicator {
+
+    private val uptime = meterRegistry.get("process.uptime").gauge();
+
+    override fun health(): Mono<Health> {
+        if (uptime.value() < 60) {
+            return Mono.just(Health.up().build())
+        }
+        return tradeKafkaProducer.metricsFromProducer()
+                .filter { o -> o.t1.name() == "request-rate" }
+                .map { o -> o.t2.metricValue() as Double }
+                .next()
+                .map { o ->
+                    if (o == 0.0) Health.down().build()
+                    else Health.up()
+                            .withDetail("uptime", uptime.value())
+                            .withDetail("request-rate", o).build()
+                }
+    }
+
+}
