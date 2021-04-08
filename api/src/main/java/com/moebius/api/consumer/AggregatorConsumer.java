4843a95dbd3d72c7d4b99115e40bad5a58f23b7e
pointware <pwangkk@gmail.com>
2021-04-09 01:46:06 +0900

    add profile in kafka consumer

--- api/src/main/java/com/moebius/api/consumer/AggregatorConsumer.java
+++ api/src/main/java/com/moebius/api/consumer/AggregatorConsumer.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,7 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class AggregatorConsumer implements ConsumerSeekAware {
 
