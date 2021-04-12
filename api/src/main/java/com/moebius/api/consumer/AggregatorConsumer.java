d8663f0d45a34c80dac872dcf4bd2c54f0e5968a
pointware <pwangkk@gmail.com>
2021-04-13 00:57:01 +0900

    add health indicator  - add warm-up check method  - implement health indicator

--- api/src/main/java/com/moebius/api/consumer/AggregatorConsumer.java
+++ api/src/main/java/com/moebius/api/consumer/AggregatorConsumer.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.TopicPartition;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.ConsumerSeekAware;
@@ -16,6 +15,8 @@ import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -26,11 +27,23 @@ public class AggregatorConsumer implements ConsumerSeekAware {
 
     private final Cache<String, TradeStats> tradeStatsCache;
     private final ObjectMapper mapper;
+    private final Map<TopicPartition, Long> topicPartitionOffsetMap;
+    private CountDownLatch countDownLatch;
 
     @KafkaListener(topics = "${spring.kafka.consumer.topics}", groupId = "${spring.kafka.consumer.group-id}")
     public void message(ConsumerRecord<String, String> data) throws JsonProcessingException {
+
         var message = mapper.readValue(data.value(), TradeStats.class);
         putMessageCache(message);
+
+        var topicPartition = new TopicPartition(data.topic(), data.partition());
+        topicPartitionOffsetMap.computeIfPresent(topicPartition, (key, value) -> {
+            if (data.offset() >= value) {
+                countDownLatch.countDown();
+                return null;
+            }
+            return value;
+        });
     }
 
     private void putMessageCache(TradeStats tradeStats) {
@@ -39,8 +52,10 @@ public class AggregatorConsumer implements ConsumerSeekAware {
 
     @Override
     public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        this.topicPartitionOffsetMap.putAll(assignments);
         long timestamp = getConsumerStartTimestamp();
         callback.seekToTimestamp(assignments.keySet(), timestamp);
+        countDownLatch = new CountDownLatch(assignments.size());
         log.info("set timestamp:{}", timestamp);
     }
 
@@ -50,5 +65,9 @@ public class AggregatorConsumer implements ConsumerSeekAware {
 
     private long getUnixTickNow() {
         return Instant.now().getEpochSecond() * 1000;
+    }
+
+    public boolean isReady() {
+        return Optional.ofNullable(this.countDownLatch).map(o -> o.getCount() == 0L).orElse(false);
     }
 }
